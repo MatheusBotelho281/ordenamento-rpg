d848ead8b919c3ba8226b8abf1829d670c0892fd
Matheus Botelho <matheusbotelho281@gmail.com>
2021-03-31 15:24:28 -0300

    Tira matheuzola, entra yordle

--- src/components/PopUp/index.js
+++ src/components/PopUp/index.js
@@ -5,7 +5,7 @@ export default function PopUp(props) {
   return (
     <Wrapper>
       <img onClick={props.close} src='https://media.discordapp.net/attachments/501196063613648900/824773502330077205/close.png' />
-      <iframe src="https://www.youtube.com/embed/P23gCK_cBME" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/QTiaknioi9c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Wrapper>
   )
 }

1bb4fd9671bd4e3b6258c8a54edf388cffbb2e54
Matheus Botelho <79803473+MatheusBotelho281@users.noreply.github.com>
2021-05-23 21:27:28 -0300

    erro estranho

--- src/pages/innerPages/Ordenamento1.0/index.js
+++ src/pages/innerPages/Ordenamento1.0/index.js
@@ -1,7 +1,67 @@
 import React from 'react'
-import { Título, Texto, Wrapper, InnerWrapper, Imagem } from './styles.js'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+margin-top: 25px;
+iframe{
+  position: fixed;
+  top: 250px;
+  left: 55px;
+  width: 300px;
+  height: 600px;
+  transition: all 1s;
+  @media only screen and (max-width: 1560px){
+    top: 230px;
+    width: 285px;
+    height: 480px;
+  }
+  @media only screen and (max-width: 1480px){
+    top: 200px;
+    width: 250px;
+    height: 480px;
+  }
+}
+`
+const InnerWrapper = styled.div`
+width: 45%;
+display: flex;
+align-items: start;
+justify-content: center;
+flex-direction: column;
+`
+
+const Post = styled.div`
+display: flex;
+flex-direction: column;
+font-family: "Nunito", sans-serif;
+`
+
+const Texto = styled.p`
+color: white;
+font-family: "Nunito", sans-serif;
+`
+
+const Título = styled.h3`
+color: lightgray;
+font-family: "Nunito", sans-serif;
+`
+
+const Imagem = styled.img`
+width: ${props => props.largura};
+height: ${props => props.altura};
+max-width: 620px;
+max-height: 370px;
+border: 1px solid white;
+border-radius: 3px;
+`
 
 export default function Ordenamento1() {
+
   return (
     <>
       <Wrapper>

cbf2d7e1be93ac5ccf42a9e25f3e6979955cd753
Matheus Botelho <79803473+MatheusBotelho281@users.noreply.github.com>
2021-06-03 21:33:31 -0300

    Att Witcher

--- src/pages/innerPages/The Witcher/index.js
+++ src/pages/innerPages/The Witcher/index.js
@@ -106,11 +106,11 @@ export default function TheWitcher() {
             Poucos detalhes faltando para finalizar toda a história para o segundo arco, <span style={{ fontWeight: "700", color: "lightblue" }}>A Caçada Selvagem</span>.
           </Texto>
           <Texto>
-            O sistema usado será o The Witcher 1.9, assim que o balanceamento estiver finalizado ficará disponível junto da ficha na área de Arquivos. Para os bruxos,
-            a lista de poções não sofrerá tantas modificações, o Bestiário será atualizado aos poucos, muito para manter uma dinâmica de aprendizado para os bruxos dentro do jogo.
+            O sistema usado será o The Witcher RPG Oficial. A ficha será disponibilizada para download algum tempo antes do RPG, as fichas serão
+            montadas em conjunto com o mestre, o sistema é bem complexo e é bom ter um acompanhamento para montar e modificar os status dos personagens.
           </Texto>
           <Texto>
-            Muito mudou desde os acontecimentos da Era do Renascer, onde o panteão foi alterado e as linhas temporais estabilizadas. Mas existem males obscuros, escondidos
+            Muito mudou desde os acontecimentos da Era do Renascer, onde o panteão foi levado ao utilizado nos livros e jogos e as linhas temporais estabilizadas. Mas existem males obscuros, escondidos
             da visão de todos apenas aguardando o momento para agir. Muitos tem visto a Caçada Selvagem cortar os céus, buscam algo sem tirarem tempo para descansar,
             creem que trazem o fim de tudo, o <span style={{ color: "red" }}>Ragnarok</span>.
           </Texto>
@@ -121,11 +121,6 @@ export default function TheWitcher() {
             às cartas da Escola do Dragão.
           </Texto>
           <Imagem src="./escolaDragao.jpeg" loading="lazy" alt="Escola do Dragão"/>
-          <Texto>
-            Kaedwen tem movimentado soldados, o clima de guerra se torna cada vez mais tenso, a décadas ninguém tentava invadir o país, mas Radovid V da Redania
-            tem se provado sendo um louco, mas excelente estrategista. Todos os reinos estão em um clima desagradavel, e muitos dos feiticeiros tem
-            saído em busca de respostas sobre a estranha leitura mágica em Okami. 
-          </Texto>
         </InnerWrapper>
       </Wrapper>
     </>

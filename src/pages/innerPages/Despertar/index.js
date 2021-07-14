1695ed922ff68f62854a79a741c92ec24c156462
Matheus Botelho <79803473+MatheusBotelho281@users.noreply.github.com>
2021-07-14 16:11:41 -0300

    Campeão Gélido Post Número 2

--- src/pages/innerPages/Despertar/index.js
+++ src/pages/innerPages/Despertar/index.js
@@ -102,6 +102,52 @@ export default function Despertar() {
         <iframe className="discord" src="https://discord.com/widget?id=517026316378308629&theme=dark" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         <InnerWrapper>
           <Título>
+            O Campeão Gélido |<span style={{ fontSize: "23px" }}> 14/07/2021</span>
+          </Título>
+          <Texto>
+            “Na Grande Era da Escuridão, a mais de 1000 anos, a esperança da Aliança estava fraca, havia perdido muitos soldados na
+            luta contra os males que afligiam as Terras Livres. Elfos e humanos lutavam lado a lado, anãos e gnomos fortaleciam as
+            linhas de defesa e todos os outros povos se misturavam em um aglomerado de guerreiros prontos para entregar tudo o que
+            tinham. Tarrasques e gigantes marchavam, o Rei Litch marchava à frente controlando a todos, invocando o que fosse necessário
+            com sua vã magia.
+          </Texto>
+          <Texto>
+            Em meio a tanto desespero um dos guerreiros, Draron SunBlood, elfo das baixas castas, decidiu partir junto de uma comitiva
+            atrás de uma velha lenda. Nos reinos gelados, além do Mar Esquecido, existe uma fonte de poder para aquele que puder
+            derrubar o Senhor Gélido, guardião da porta para os reinos mágicos de Ranemalor, fonte de toda magia bruta do mundo.
+            Durante um ano inteiro viajaram por terra, água e ar, enfrentaram criaturas nunca antes vistas, alguns desses membros
+            do grupo foram perdidos no percurso, no final, apenas 3 dos 11 chegaram ao destino.
+          </Texto>
+          <Imagem src="./reino-de-gelo.jpg" />
+          <Texto>
+            O reino estava destruído, o gelo e o frio intenso tinham matado a todos ali, a arquitetura era peculiar, não dava para
+            saber a quanto tempo aquele povo havia vivido ali. Caminharam até o velho castelo, no topo da montanha, ali encontraram
+            um velho e decrépito homem, curvado enquanto dormia em paz, em sua cabeça uma coroa feita de um gelo negro, era ela a
+            chave entre esse plano e o plano magico.
+          </Texto>
+          <Texto>
+            Atacaram o Senhor Gélido, mas este resistiu aos golpes, girando uma espada de gelo puro em resposta aos ataques, atingiu
+            um dos aliados de Draron, a batalha se estendeu por muito tempo, em um momento o Senhor de Gelo acabou vacilando na
+            hora de dar um passo de ajuste, Draron e um de seus aliados se arremessaram em direção do velho, cortando fora sua cabeça
+            com muito esforço.
+          </Texto>
+          <Texto>
+            Retornaram para a sua terra, os três estavam machucados, carregavam a coroa junto, a chamaram de Chave do Verdadeiro Gelo.
+            Chegaram de volta depois de 2 anos de viagem, resolveram ir pelo caminho mais seguro, evitando conflitos desnecessários.
+            Quando viram o que havia ocorrido, já era quase tarde demais, as forças inimigas avançaram muitora pidamente, tinham agora
+            apenas uma parte de dez do continente, estavam lutando de um para dois mil, a derrota era inevitável, pelo menos até aquele
+            momento.
+          </Texto>
+          <Texto>
+            Draron se pôs em frente ao exército, posicionou a coroa na cabeça e a magia fluiu em seu corpo, a guerra foi vencida apenas
+            depois de mais 3 anos de conflitos, dessa vez com a magia ao lado da Aliança. Depois de tudo isso, Draron, agora chamado
+            de Campeão Gélido, se pôs em exílio, a magia da coroa havia se prendido a sua alma permanentemente, e ele não seria forte
+            o suficiente para se manter distante da Corrupção Arcana. Alguns dizem que ele está em outro plano, outros que ele voltou
+            para as terras de onde havia adquirido a coroa, no final das contas, ninguém saberá." <br/>
+            <span style={{color: "lightgray"}}>Trecho do antigo Livro do Marco Gélido, por Harold o Grande.</span>
+          </Texto>
+          <div style={{ width: "100%", height: "2px", backgroundColor: "gray", margin: "30px 0 30px 0" }}></div>
+          <Título>
             Despertar do Campeão Gélido
           </Título>
           <Texto>

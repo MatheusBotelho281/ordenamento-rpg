cd5533abf4af8a167b52913381900d34325b18e7
Matheus Botelho <79803473+MatheusBotelho281@users.noreply.github.com>
2021-08-23 13:45:38 -0300

    lich king

--- src/pages/innerPages/Despertar/index.js
+++ src/pages/innerPages/Despertar/index.js
@@ -102,6 +102,54 @@ export default function Despertar() {
         <iframe className="discord" src="https://discord.com/widget?id=517026316378308629&theme=dark" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         <InnerWrapper>
           <Título>
+            A História até aqui. |<span style={{ fontSize: "23px" }}> 23/08/2021</span>
+          </Título>
+          <Texto>
+            Passamos por muitas eras, sempre nos reconstruindo, desde o primeiro dia da Era da Criação, onde tudo era incerto,
+            apenas poucos elfos andavam pelas terras e anãos se estabeleciam na beirada de montanhas, ambos ignorando o levante
+            da raça humana, e assim pouco mais de 300 anos se passaram, até a Expansão Arcana.
+          </Texto>
+          <Texto>
+            Onde poucos tinham acesso a magia, agora muitos acordaram com novos talentos, capacidades arcanas novas, humanos
+            podiam usufruir das artes antigas dos elfos, enquanto anãos desenvolviam uma maneira peculiar de lidar com a magia,
+            e assim se passaram mais 500 anos, com os humanos se multiplicando cada vez mais. No meio da era novos povos surgiam,
+            novas raças saiam de seus recintos, gnomos e orcs apareciam aos poucos em meio a população, guerras começavam a surgir
+            nos pensamentos de todos os povos. E assim foi a Era da Perdição.
+          </Texto>
+          <Texto>
+            O que aconteceu ao final daquela era é incerto, poucos documentos sobreviveram a ação do tempo, aquilo que foi chamado de Era
+            dos Deuses quase se demonstrou como a destruição de toda a vida do mundo conhecido. Aos poucos alguns indivíduos se entregavam a
+            certas “vontades” primordiais, como vida, morte, guerra, amor, vaidade e muitos outros, tudo uma grande troca, o livre-arbítrio e
+            serventia até a morte por poder muito além da compreensão.
+          </Texto>
+          <Texto>
+            Tal era durou pouco, em apenas 150 anos algo maior do que o surgimentos dos deuses tomou o palco para si, um mago, certamente
+            o mais poderoso de sua época, seu nome se perdeu em meio ao caos da história antiga, o que se sabe é que o arcanista descobriu
+            uma antiga ruína, datada de um período anterior aos elfos e aos anãos, com ensinamentos sobre magia, essa tão poderosa que
+            era capaz de deturpar a realidade, e tal capacidade o levou a insanidade, em alguns textos é dito que algo tomou a mente
+            e corpo dele, o tornando refém dentro de si mesmo. Com tal poder ele começou uma insurgência, lentamente ganhando influência
+            pelas terras, construindo um exército sob o nome do Rei Lich.
+          </Texto>
+          <Imagem src="./lichking.jpg"></Imagem>
+          <Texto>
+            A guerra durou séculos, avançando lentamente, em certo ponto o poder acumulado pelo mago era tamanho que os deuses foram
+            obrigados a se juntar para construir guerreiros perfeitos, capazes de derrotar as inúmeras hordas do inimigo. Os chamaram
+            de dragões, e foram esses os que garantiram tempo o suficiente para a retirada de muitos inocentes das grandes cidades.
+            Mas o Lich não gostou de ser superado em quesito de tropas, assim, iniciou a criação de seus próprios dragões, esses diferentes,
+            o primeiro acabou falhando, seu poder saiu do controle e ele se tornou um devorador de mundos, o que hoje é chamado de Tarrasque,
+            já os outros, eram cromáticos, enquanto os dragões dos deuses eram de cores metálicas.
+          </Texto>
+          <Texto>
+            E assim a guerra continuou, com os dragões do Rei Lich a vantagem aparente dos deuses desapareceu, o poder deles era menor,
+            mas em quantidade podiam vencer facilmente. O Conselho de Hel, criado para definir sobre a criação dos dragões, definiu
+            que uma atitude de emergência deveria ser tomada, enviar soldados em uma missão suicida até as velhas Terras Congeladas
+            em busca de um poder que não passava de lenda até aquele momento.
+          </Texto>
+          <Texto>
+            E assim o <span style={{color: "cyan"}}>Campeão Gélido</span> foi desperto.
+          </Texto>
+          <div style={{ width: "100%", height: "2px", backgroundColor: "gray", margin: "30px 0 30px 0" }}></div>
+          <Título>
             O Campeão Gélido |<span style={{ fontSize: "23px" }}> 14/07/2021</span>
           </Título>
           <Texto>
@@ -143,8 +191,8 @@ export default function Despertar() {
             depois de mais 3 anos de conflitos, dessa vez com a magia ao lado da Aliança. Depois de tudo isso, Draron, agora chamado
             de Campeão Gélido, se pôs em exílio, a magia da coroa havia se prendido a sua alma permanentemente, e ele não seria forte
             o suficiente para se manter distante da Corrupção Arcana. Alguns dizem que ele está em outro plano, outros que ele voltou
-            para as terras de onde havia adquirido a coroa, no final das contas, ninguém saberá." <br/>
-            <span style={{color: "lightgray"}}>Trecho do antigo Livro do Marco Gélido, por Harold o Grande.</span>
+            para as terras de onde havia adquirido a coroa, no final das contas, ninguém saberá." <br />
+            <span style={{ color: "lightgray" }}>Trecho do antigo Livro do Marco Gélido, por Harold o Grande.</span>
           </Texto>
           <div style={{ width: "100%", height: "2px", backgroundColor: "gray", margin: "30px 0 30px 0" }}></div>
           <Título>
@@ -152,7 +200,7 @@ export default function Despertar() {
           </Título>
           <Texto>
             Aventura de D&D para quem quer começar no jogo, ou até mesmo já teva alguma experiência com RPG de mesa.
-            </Texto>
+          </Texto>
           <Texto>
             Existem algumas raças e classes que moldam o jogo, "Mas o que exatamente seria essa parada?", lhe respondo, digamos que
             você queira fazer um Elfo Arqueiro, então eu diria que uma boa ideia seria usar a <span style={{ fontWeight: "700", color: "green" }}>Classe de Patrulheiro</span> com
